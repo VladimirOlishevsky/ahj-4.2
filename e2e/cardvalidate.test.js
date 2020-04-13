8cbe5c0f111a13a46772399c12aa3a3116a75504
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2020-04-13 16:55:38 +0300

    update puppeteer

--- e2e/cardvalidate.test.js
+++ e2e/cardvalidate.test.js
@@ -1,11 +1,25 @@
 import puppetteer from 'puppeteer';
 
-jest.setTimeout(300000); // default puppeteer timeout
+const childProcess = require('child_process');
+
+const server = childProcess.fork(`${__dirname}/test-server.js`);
+
+jest.setTimeout(30000); // default puppeteer timeout
 describe('Test form', () => {
     let browser = null;
     let page = null;
-    const baseUrl = 'http://localhost:9000/';
+    const baseUrl = 'http://localhost:9999';
     beforeAll(async() => {
+        await new Promise((resolve, reject) => {
+            server.on('error', () => {
+                reject();
+            });
+            server.on('message', (message) => {
+                if (message === 'ok') {
+                    resolve();
+                }
+            });
+        });
         browser = await puppetteer.launch({
             headless: false, // show gui
             slowMo: 100,
@@ -15,19 +29,17 @@ describe('Test form', () => {
     });
     afterAll(async() => {
         await browser.close();
+        server.kill();
     });
     // test code here (next page)
-    describe('Test form', () => {
+    test('test form', async() => {
+        await page.goto(baseUrl);
+        const form = await page.$('[data-widgets=credit-card-validator]');
+        const input = await form.$('[data-name=input-number]');
+        await input.type('5469550044928767');
+        const submit = await form.$('.card-validator__submit');
+        submit.click();
+        await page.waitForSelector('.card-validator__valid');
 
-        test('test form', async() => {
-            console.log(page)
-            await page.goto('http://localhost:9000/');
-            const form = await page.$('[data-widgets=credit-card-validator]');
-            const input = await form.$('[data-name=input-number]');
-            await input.type('5469550044928767');
-            const submit = await form.$('[class=card-validator__submit]');
-            submit.click();
-            await page.waitForSelector('[class=card-validator__valid');
-        });
     });
 });
